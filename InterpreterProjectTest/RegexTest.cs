00923c637a9c9150207a91d3efe26ff0f9bfd56f
Paul Saikko <paul.saikko@cs.helsinki.fi>
2014-02-28 22:46:54 +0200

    Created test project

---
+++ InterpreterProjectTest/RegexTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterpreterProject;
+using System.Collections.Generic;
+
+namespace InterpreterProjectTest
+{
+    [TestClass]
+    public class RegexTest
+    {
+        [TestMethod]
+        public void Regex_CharacterTest()
+        {
+            Regex a = Regex.Character('a');
+
+            TokenClass aClass = new TokenClass("a", a);
+
+            DFA automaton = a.ConstructDFA();
+
+            string text = "aaaaa";
+
+            Scanner sc = new Scanner(automaton);
+            List<Token> tokens = sc.Tokenize(text);
+
+            Assert.AreEqual(tokens.Count, 5);
+            for (int i = 0; i < 5; i++)
+                Assert.AreEqual(tokens[0].lexeme, "a");
+        }
+    }
+}
