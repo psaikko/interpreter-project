463f76347c1af673c5696ca05ebb77e34b955662
Paul Saikko <paul.saikko@cs.helsinki.fi>
2014-02-28 23:53:00 +0200

    Test for regex concatenation

--- InterpreterProjectTest/RegexTest.cs
+++ InterpreterProjectTest/RegexTest.cs
@@ -12,9 +12,7 @@ namespace InterpreterProjectTest
         public void Regex_CharacterTest()
         {
             Regex a = Regex.Character('a');
-
             TokenClass aClass = new TokenClass("a", a);
-
             DFA automaton = a.ConstructDFA();
 
             string text = "aaaaa";
@@ -22,9 +20,27 @@ namespace InterpreterProjectTest
             Scanner sc = new Scanner(automaton);
             List<Token> tokens = sc.Tokenize(text);
 
-            Assert.AreEqual(tokens.Count, 5);
+            Assert.AreEqual(5, tokens.Count);
             for (int i = 0; i < 5; i++)
                 Assert.AreEqual(tokens[0].lexeme, "a");
+        }
+
+        [TestMethod]
+        public void Regex_ConcatTest()
+        {
+            Regex ab = Regex.Concat("ab");
+            TokenClass abClass = new TokenClass("ab", ab);
+            Console.WriteLine(ab);
+            DFA automaton = ab.ConstructDFA();
+
+            string text = "ababab";
+
+            Scanner sc = new Scanner(automaton);
+            List<Token> tokens = sc.Tokenize(text);
+
+            Assert.AreEqual(3, tokens.Count);
+            for (int i = 0; i < 5; i++)
+                Assert.AreEqual(tokens[0].lexeme, "ab");
         }
     }
 }
