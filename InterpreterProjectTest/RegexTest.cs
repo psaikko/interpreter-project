47ecdb41d97b65ed95e7f039a32138206299dabb
Paul Saikko <paul.saikko@cs.helsinki.fi>
2014-03-01 00:29:03 +0200

    Regex tests for Union and Range

--- InterpreterProjectTest/RegexTest.cs
+++ InterpreterProjectTest/RegexTest.cs
@@ -22,7 +22,7 @@ namespace InterpreterProjectTest
 
             Assert.AreEqual(5, tokens.Count);
             for (int i = 0; i < 5; i++)
-                Assert.AreEqual(tokens[0].lexeme, "a");
+                Assert.AreEqual(tokens[i].lexeme, "a");
         }
 
         [TestMethod]
@@ -30,7 +30,6 @@ namespace InterpreterProjectTest
         {
             Regex ab = Regex.Concat("ab");
             TokenClass abClass = new TokenClass("ab", ab);
-            Console.WriteLine(ab);
             DFA automaton = ab.ConstructDFA();
 
             string text = "ababab";
@@ -39,8 +38,46 @@ namespace InterpreterProjectTest
             List<Token> tokens = sc.Tokenize(text);
 
             Assert.AreEqual(3, tokens.Count);
-            for (int i = 0; i < 5; i++)
-                Assert.AreEqual(tokens[0].lexeme, "ab");
+            for (int i = 0; i < 3; i++)
+                Assert.AreEqual(tokens[i].lexeme, "ab");
+        }
+
+        [TestMethod]
+        public void Regex_RangeTest()
+        {
+            Regex az = Regex.Range('a', 'z');
+            TokenClass azClass = new TokenClass("az", az);
+            DFA automaton = az.ConstructDFA();
+
+            string text = "abcdefghijklmnopqrstuvwxyz";
+
+            Scanner sc = new Scanner(automaton);
+            List<Token> tokens = sc.Tokenize(text);
+
+            Assert.AreEqual(text.Length, tokens.Count);
+            for (int i = 0; i < text.Length; i++)
+                Assert.AreEqual(((char)('a'+i)).ToString(), tokens[i].lexeme);
+        }
+
+        [TestMethod]
+        public void Regex_UnionTest()
+        {
+            Regex aa = Regex.Concat("aa");
+            Regex ab = Regex.Union("ab");
+            TokenClass aaClass = new TokenClass("aa", aa);
+            TokenClass abClass = new TokenClass("a|b", ab);
+            Regex combined = aa.Union(ab);
+            DFA automaton = combined.ConstructDFA();
+
+            string text = "aababaabaa";
+
+            Scanner sc = new Scanner(automaton);
+            List<Token> tokens = sc.Tokenize(text);
+
+            string[] expectedTokens = {"aa","b","a","b","aa","b","aa"};
+            Assert.AreEqual(expectedTokens.Length, tokens.Count);
+            for (int i = 0; i < expectedTokens.Length; i++)
+                Assert.AreEqual(expectedTokens[i], tokens[i].lexeme);
         }
     }
 }
