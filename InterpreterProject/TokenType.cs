794ffb78b7ea07e61b2869dba057b17825692214
Paul Saikko <paul.saikko@cs.helsinki.fi>
2014-03-01 23:17:14 +0200

    Added missing files

---
+++ InterpreterProject/TokenType.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpreterProject
+{
+    public class TokenType
+    {
+        public enum Priority { Whitespace, Default, Keyword };
+
+        public static readonly TokenType ERROR = new TokenType("ERROR", Regex.None());
+        public static readonly TokenType EOF = new TokenType("EOF", Regex.Character(TokenAutomaton.EOF));
+
+        public readonly String name;
+        public readonly Priority priority;
+        readonly Regex regex;
+
+        public TokenType(String name, Regex regex, Priority priority = Priority.Default)
+        {
+            this.name = name;
+            this.regex = regex;
+            this.priority = priority;
+            regex.DefineTokenClass(this);
+        }
+
+        public Token CreateToken(String lexeme)
+        {
+            return new Token(this, lexeme);
+        }
+
+        public TokenAutomaton Automaton()
+        {
+            return regex.ConstructAutomaton();
+        }
+
+        public static TokenAutomaton CombinedAutomaton(params TokenType[] ts)
+        {
+            Regex[] rs = new Regex[ts.Length];
+            for (int i = 0; i < ts.Length; i++)
+                rs[i] = ts[i].regex;
+            Regex combined = Regex.Union(rs);
+            return combined.ConstructAutomaton();
+        }
+    }
+}
---
+++ InterpreterProjectTest/ScannerTest.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using InterpreterProject;
+
+namespace InterpreterProjectTest
+{
+    [TestClass]
+    public class ScannerTest
+    {
+        [TestMethod]
+        public void Scanner_ErrorTest()
+        {
+            Regex a = Regex.Character('a');
+            TokenType aToken = new TokenType("a", a);
+            TokenAutomaton automaton = aToken.Automaton();
+
+            Scanner sc = new Scanner(automaton);
+
+            string text = "ccaacaacc";
+
+            List<Token> tokens = sc.Tokenize(text);
+
+            string[] expectedTokens = {"c","c","a","a","c","a","a","c","c"};
+            Assert.AreEqual(expectedTokens.Length, tokens.Count);
+            for (int i = 0; i < expectedTokens.Length; i++)
+            {
+                Assert.AreEqual(expectedTokens[i], tokens[i].lexeme);
+                if (tokens[i].lexeme == "a")
+                    Assert.AreEqual(aToken, tokens[i].type);
+                else
+                    Assert.AreEqual(TokenType.ERROR, tokens[i].type);
+            }
+        }
+
+        [TestMethod]
+        public void Scanner_LineCommentTest()
+        {
+            // We can recognize line comments with tokens marked whitespace 
+            // so they aren't returned by the scanner
+
+            Regex a = Regex.Character('a').Star();
+            Regex lineComment = Regex.Concat("//").Concat(Regex.Not('\n').Star());
+            Regex whitespace = Regex.Union(" \t\n").Star();
+            TokenType ttWhitespace = new TokenType("Whitespace", whitespace, priority: TokenType.Priority.Whitespace);
+            TokenType ttA = new TokenType("a*", a);
+            TokenType ttLineComment = new TokenType("line comment", lineComment, priority: TokenType.Priority.Whitespace);
+            TokenAutomaton automaton = TokenType.CombinedAutomaton(ttA, ttLineComment, ttWhitespace);
+
+            Scanner sc = new Scanner(automaton);
+
+            string text = "aaa//aaa\n"+
+                          "//aaaaaaa\n"+
+                          "aaa//aaa";
+
+            List<Token> tokens = sc.Tokenize(text);
+
+            string[] expectedTokens = { "aaa", "aaa" };
+            Assert.AreEqual(expectedTokens.Length, tokens.Count);
+            for (int i = 0; i < expectedTokens.Length; i++)
+            {
+                Assert.AreEqual(expectedTokens[i], tokens[i].lexeme);
+            }
+        }
+
+        [TestMethod]
+        public void Scanner_BlockCommentTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Scanner_IntegerTest()
+        {
+            Regex whitespace = Regex.Union(" \t\n").Star();
+            Regex integer = Regex.Character('-').Maybe()
+                .Concat(Regex.Range('1', '9'))
+                .Concat(Regex.Range('0', '9').Star())
+                .Union(Regex.Character('0'));
+            TokenType ttWhitespace = new TokenType("Whitespace", whitespace);
+            TokenType ttInteger = new TokenType("integer", integer);
+            TokenAutomaton automaton = TokenType.CombinedAutomaton(ttInteger, ttWhitespace);
+
+            Scanner sc = new Scanner(automaton);
+
+            string text = "1234 0 -99 -1";
+            List<Token> tokens = sc.Tokenize(text);
+            string[] expectedTokens = { "1234", " ", "0", " ", "-99", " ", "-1" };
+
+            Assert.AreEqual(expectedTokens.Length, tokens.Count);
+            for (int i = 0; i < expectedTokens.Length; i++)
+                Assert.AreEqual(expectedTokens[i], tokens[i].lexeme);
+        }
+
+        [TestMethod]
+        public void Scanner_WhitespaceTest()
+        {
+            Regex whitespace = Regex.Union(" \t\n").Star();
+            Regex a = Regex.Character('a');
+            TokenType ttWhitespace = new TokenType("Whitespace", whitespace);
+            TokenType ttA = new TokenType("a", a);
+            TokenAutomaton automaton = TokenType.CombinedAutomaton(ttA, ttWhitespace);
+
+            Scanner sc = new Scanner(automaton);
+
+            string text = "a   a  \t\na  \n  a   \t\t   a     \n\t a ";
+            List<Token> tokens = sc.Tokenize(text);
+            string[] expectedTokens = { "a", "   ", "a", "  \t\n", "a", "  \n  ", "a", "   \t\t   ", "a", "     \n\t ", "a", " " };
+
+            Assert.AreEqual(expectedTokens.Length, tokens.Count);
+            for (int i = 0; i < expectedTokens.Length; i++)
+                Assert.AreEqual(expectedTokens[i], tokens[i].lexeme);
+        }
+
+        [TestMethod]
+        public void Scanner_StringTest()
+        {
+            Regex whitespace = Regex.Union(" \t\n").Star();
+            Regex str = Regex.Character('"').Concat(Regex.Not('"').Star()).Concat(Regex.Character('"'));
+            TokenType ttWhitespace = new TokenType("Whitespace", whitespace);
+            TokenType ttString = new TokenType("string", str);
+            TokenAutomaton automaton = TokenType.CombinedAutomaton(ttWhitespace, ttString);
+
+            Scanner sc = new Scanner(automaton);
+
+            string text = "\"asdf\" \"sdfg\"";
+            List<Token> tokens = sc.Tokenize(text);
+            string[] expectedTokens = { "\"asdf\"", " ", "\"sdfg\"" };
+
+            Assert.AreEqual(expectedTokens.Length, tokens.Count);
+            for (int i = 0; i < expectedTokens.Length; i++)
+                Assert.AreEqual(expectedTokens[i], tokens[i].lexeme);
+        }
+
+        [TestMethod]
+        public void Scanner_EscapeStringTest()
+        {
+            Regex whitespace = Regex.Union(" \t\n").Star();
+            // construct the regex "(\\.|[^"\\])*"
+            Regex strBegin = Regex.Character('"');
+            Regex strEnd = Regex.Character('"');
+            Regex strBody = Regex.Character('\\').Concat(Regex.Any()).Union(Regex.Not('"', '\\')).Star();
+            Regex str = strBegin.Concat(strBody).Concat(strEnd);
+            TokenType ttWhitespace = new TokenType("Whitespace", whitespace);
+            TokenType ttString = new TokenType("string", str);
+            TokenAutomaton automaton = TokenType.CombinedAutomaton(ttWhitespace, ttString);
+
+            Scanner sc = new Scanner(automaton);
+
+            string text = "\"as\\ndf\\\"\" \"sdfg\\\\\" \"\\\\\"\"";
+            List<Token> tokens = sc.Tokenize(text);
+            
+            string[] expectedTokens = { "\"as\\ndf\\\"\"", " ", "\"sdfg\\\\\"" , " ", "\"\\\\\"", "\""};
+
+            Assert.AreEqual(expectedTokens.Length, tokens.Count);
+            for (int i = 0; i < expectedTokens.Length; i++)
+                Assert.AreEqual(expectedTokens[i], tokens[i].lexeme);
+        }
+
+        [TestMethod]
+        public void Scanner_TokenPriorityTest()
+        {
+            // keywords should be matched over regular tokens and whitespace should not be returned
+            Regex keywords = Regex.Union(Regex.Concat("int"), Regex.Concat("bool"));
+            Regex words = Regex.Range('a', 'z').Plus();
+            Regex whitespace = Regex.Union(" \t\n").Star();
+            TokenType ttKeyword = new TokenType("keyword", keywords, priority: TokenType.Priority.Keyword);
+            TokenType ttWords = new TokenType("words", words);
+            TokenType ttWhitespace = new TokenType("Whitespace", whitespace, priority: TokenType.Priority.Whitespace);
+            TokenAutomaton automaton = TokenType.CombinedAutomaton(ttKeyword, ttWords, ttWhitespace);
+
+            Scanner sc = new Scanner(automaton);
+
+            string text = "in int ints bool bools boo";
+            List<Token> tokens = sc.Tokenize(text);
+
+            string[] expectedTokens = { "in", "int", "ints", "bool", "bools", "boo"};
+
+            Assert.AreEqual(expectedTokens.Length, tokens.Count);
+            for (int i = 0; i < expectedTokens.Length; i++)
+                Assert.AreEqual(expectedTokens[i], tokens[i].lexeme);
+        }
+
+        private Scanner CreateMiniPLScanner()
+        {
+            Regex whitespace = Regex.Union(" \t\n").Star();
+            Regex str = Regex.Character('"')
+                        .Concat(Regex.Character('\\')
+                            .Concat(Regex.Any())
+                            .Union(Regex.Not('"', '\\')).Star())
+                        .Concat(Regex.Character('"'));
+            Regex binaryOp = Regex.Union("+-*/<=&");
+            Regex unaryOp = Regex.Character('!');
+            Regex keyword = Regex.Union(
+                Regex.Concat("var"), Regex.Concat("for"), Regex.Concat("end"), Regex.Concat("in"),
+                Regex.Concat("do"), Regex.Concat("read"), Regex.Concat("print"), Regex.Concat("int"),
+                Regex.Concat("string"), Regex.Concat("bool"), Regex.Concat("assert"));
+            Regex lParen = Regex.Character('(');
+            Regex rParen = Regex.Character(')');
+            Regex colon = Regex.Character(':');
+            Regex semicolon = Regex.Character(';');
+            Regex assign = Regex.Concat(":=");
+            Regex dots = Regex.Concat("..");
+            Regex ident = Regex.Union(Regex.Range('A', 'Z'), 
+                                      Regex.Range('a', 'z'))
+                            .Concat(Regex.Union(Regex.Range('A', 'Z'),
+                                                Regex.Range('a', 'z'),
+                                                Regex.Range('0', '9'),
+                                                Regex.Character('_')));
+
+            TokenType ttWhitespace = new TokenType("whitespace", whitespace);
+            TokenType ttString = new TokenType("string", str);
+            TokenType ttBinaryOp = new TokenType("binary op", binaryOp);
+            TokenType ttUnaryOp = new TokenType("unary op", unaryOp);
+            TokenType ttKeyword = new TokenType("keyword", keyword);
+            TokenType ttRightParen = new TokenType("right paren", rParen);
+            TokenType ttLeftParen = new TokenType("left paren", lParen);
+            TokenType ttColon = new TokenType("colon", colon);
+            TokenType ttSemicolon = new TokenType("semicolon", semicolon);
+            TokenType ttAssign = new TokenType("assignment", assign);
+            TokenType ttDots = new TokenType("range dots", dots);
+            TokenType ttIdent = new TokenType("identifier", ident);
+
+            TokenAutomaton automaton = TokenType.CombinedAutomaton(
+                ttWhitespace, ttString, ttBinaryOp, ttUnaryOp, ttKeyword, ttRightParen, ttLeftParen,
+                ttColon, ttSemicolon, ttAssign, ttDots, ttIdent);
+
+            return new Scanner(automaton);
+        }
+
+        [TestMethod]
+        public void Scanner_MiniPLTest_Example1()
+        {
+
+
+            string text = "var X : int := 4 + (6 * 2);\n"+
+                          "print X;";
+        }
+
+        [TestMethod]
+        public void Scanner_MiniPLTest_Example2()
+        {
+            string text = "var nTimes : int := 0;\n" +
+                          "print \"How many times?\";\n" +
+                          "read nTimes;\n" +
+                          "var x : int;\n" +
+                          "for x in 0..nTimes-1 do\n" +
+                          "     print x;\n" +
+                          "     print \" : Hello, World!\n\";\n" +
+                          "end for;\n" +
+                          "assert (x = nTimes);";
+        }
+
+        [TestMethod]
+        public void Scanner_MiniPLTest_Example3()
+        {
+            string text = "print \"Give a number\";\n" +
+                          "var n : int;\n" +
+                          "read n;\n" +
+                          "var f : int := 1;\n" +
+                          "var i : int;\n" +
+                          "for i in 1..n do\n" +
+                          "    f := f * i;\n" +
+                          "end for;\n" +
+                          "print \"The result is: \";\n" +
+                          "print f;";
+        }
+    }
+}
